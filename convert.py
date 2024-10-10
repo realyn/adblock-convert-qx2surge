5007c5abdbaf5a43ea48ef4121e4ad627dbde131
realyn <realyn@qq.com>
2024-10-10 10:25:25 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -15,23 +15,28 @@ def convert_to_surge(qx_content):
     
     for line in qx_content.split('\n'):
         line = line.strip()
+        if line.startswith('#'):
+            continue
         if line in sections:
             current_section = line
-        elif line and current_section:
-            if current_section == "[URL Rewrite]":
-                if "reject" in line:
-                    parts = line.split()
-                    sections[current_section].append(f"{parts[0]} - reject")
-            elif current_section == "[Map Local]":
-                if "data=" in line:
-                    sections[current_section].append(line)
-            elif current_section == "[Script]":
-                if "type=http-response" not in line:
-                    line = line.replace("script-response-body", "type=http-response,requires-body=1,max-size=0,script-path=")
+            continue
+        if not line or not current_section:
+            continue
+        
+        if current_section == "[URL Rewrite]":
+            if "reject" in line:
+                parts = line.split()
+                sections[current_section].append(f"{parts[0]} - reject")
+        elif current_section == "[Map Local]":
+            if "data=" in line:
                 sections[current_section].append(line)
-            elif current_section == "[MITM]":
-                if line.startswith("hostname"):
-                    sections[current_section].append(line.replace("hostname = ", "hostname = %APPEND% "))
+        elif current_section == "[Script]":
+            if "type=http-response" not in line:
+                line = line.replace("script-response-body", "type=http-response,requires-body=1,max-size=0,script-path=")
+            sections[current_section].append(line)
+        elif current_section == "[MITM]":
+            if line.startswith("hostname"):
+                sections[current_section].append(line.replace("hostname = ", "hostname = %APPEND% "))
 
     for section, content in sections.items():
         if content:
